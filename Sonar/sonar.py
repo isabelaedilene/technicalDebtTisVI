9d31a6b77c25a62ee244e26f5a05ce211d94ece5
Isabela Edilene <isabelaedilene@gmail.com>
2020-06-13 13:38:40 -0300

    arquivos faltantes

--- Sonar/sonar.py
+++ Sonar/sonar.py
@@ -8,22 +8,22 @@ from csv import writer
 from json import dumps
 from requests.auth import HTTPBasicAuth
 
-BASE_PATH = "C:\\Users\\Isabela Edilene\\technicalDebtTisVI\\Sonar"
+BASE_PATH = "C:\\Users\\Isabela Edilene\\GitHub\\technicalDebtTisVI\\Sonar"
 
 def createSonarProject(name, url): 
 	print(f"INFO: Clonando o repositório {name}")
-	git.Git(f"{BASE_PATH}\\Reositorios").clone(url)
+	git.Git(f"{BASE_PATH}\\Repositorios").clone(url)
 
 	print(f"INFO: Criando projeto {name} no Sonar")
 	os.chdir(f"{BASE_PATH}\\Repositorios\\{name}") 
-	subprocess.call(f'sonar-scanner.bat -D"sonar.projectKey={name}" -D"sonar.sources=." -D"sonar.host.url=http://localhost:9000" -D"sonar.login=dc9559a7c6abfe40fcf7fc218bd80729d4f12ed6" -D"sonar.exclusions=**/*.java"', shell= True)
+	subprocess.call(f'sonar-scanner.bat -D"sonar.projectKey={name}" -D"sonar.sources=." -D"sonar.host.url=http://localhost:9000" -D"sonar.login=dc9559a7c6abfe40fcf7fc218bd80729d4f12ed6" -D"sonar.exclusions=**/*.java, **/*.js"', shell= True)
 
 def requestSonarComponents(projectKey, page):
 	response = requests.get(
 	            'http://localhost:9000/api/measures/component_tree',
 	            auth   = HTTPBasicAuth(username="admin", password="admin"),
 	            verify = False,
-	            params = {f'component': {projectKey}, 'ps' :'500', 'p' :{page}, 'metricKeys': 'code_smells,sqale_index,sqale_debt_ratio'})
+	            params = {f'component': {projectKey}, 'ps' :'500', 'p' :{page}, 'metricKeys': 'ncloc,complexity,cognitive_complexity,code_smells'})
 	responseJson = response.json()
 	return responseJson
 
@@ -47,18 +47,26 @@ def analyzeSonarComponents(projectKey):
 
 	if totalItens > 500:
 		neededRequests = round(totalItens / 500) + 1
-		for i in range(2, neededRequests):
+		for i in range(2, neededRequests + 1):
 			response = requestSonarComponents(projectKey, i)
 			allResults += response["components"]
 
+	msg = f"O PROJETO {projectKey} ESTÁ VAZIO"
 	if(len(allResults) == 0):
-		log(f"O projeto {projectKey} não retornou resultados. \n")
+		log(msg)
 	else:
-		with open(f'{BASE_PATH}\\analiseSonar.csv','a') as csv_file:
-			for component in allResults:
-				if component["measures"][0]["value"] != "0" and (".py" in component["path"]):
+		with open(f'{BASE_PATH}\\analiseSonar.csv','a', newline = '', encoding="utf-8") as csv_file:
+			for component in allResults:				
+				if component["qualifier"] == "FIL" and component["language"] == "py" and len(component["measures"]) > 3:
 					csv = writer(csv_file)  
-					csv.writerow([projectKey, f'{projectKey}/{component["path"]}'])
+					csv.writerow(
+								[projectKey,
+								f'{projectKey}/{component["path"]}',
+								component["measures"][0]["value"],
+								component["measures"][1]["value"], 
+								component["measures"][2]["value"] if component["measures"][2]["metric"] == "cognitive_complexity" else component["measures"][3]["value"],
+								component["measures"][3]["value"] if component["measures"][3]["metric"] == "code_smells" else component["measures"][2]["value"],
+					])
 
 def deleteFolder(name):
 	print("INFO: Apagando a pasta do computador")
@@ -69,9 +77,14 @@ def log(msg):
 		arquivo.write(msg)
 
 def sonarAnalysis():
+	cont = 0
+	# analyzeSonarComponents("")
 	with open(f"{BASE_PATH}\\repositoriosPython.csv", "r", encoding="utf-8") as f:
 		comments = f.read()
 		for line in comments.splitlines():
+			cont = cont + 1
+			print(f"ANALISANDO O PROJETO #{cont}")
+
 			repositorio = line.replace('"', '').split(',')
 			name = repositorio[0]
 			url = repositorio[1]
